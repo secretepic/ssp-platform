c9868f49a219d16ed3e555c7057472a2c5544bcd
cby <chenboyu@xdcool.com>
2026-02-10 19:02:53 +0800

    add readme

--- ssp-be/src/main/java/com/boyu/mvc/controller/MenuController.java
+++ ssp-be/src/main/java/com/boyu/mvc/controller/MenuController.java
@@ -3,6 +3,8 @@ package com.boyu.mvc.controller;
 import com.boyu.service.system.MenuService;
 import com.boyu.servlet.BsResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,8 +16,27 @@ public class MenuController {
     private final MenuService menuService;
 
     @RequestMapping("/list")
+//    @PreAuthorize("hasAuthority('sys:menu:list')")
     public BsResponse list() {
         return BsResponse.ok(menuService.list());
+    }
+
+    @PostMapping("/save")
+//    @PreAuthorize("hasAuthority('sys:menu:save')")
+    public BsResponse save() {
+        return BsResponse.ok();
+    }
+
+     @PostMapping("/delete")
+//    @PreAuthorize("hasAuthority('sys:menu:delete')")
+    public BsResponse delete() {
+        return BsResponse.ok();
+    }
+
+     @PostMapping("/update")
+//    @PreAuthorize("hasAuthority('sys:menu:update')")
+    public BsResponse update() {
+        return BsResponse.ok();
     }
 
 }
